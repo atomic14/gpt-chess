02acbd9b76b29f584c30e1eb574391a671be8ba4
Chris Greening <chris@cmgresearch.com>
2023-04-20 15:35:49 +0100

    Fix responsiveness

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootswatch/dist/darkly/bootstrap.min.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
